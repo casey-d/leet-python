103b4ae5a29ed794df1d8ee135918a0255e74f8f
casey-d <caseyd@alum.mit.edu>
2024-05-01 23:40:33 -0400

    Initial project

---
+++ 9palindromenumber.py
@@ -0,0 +1,13 @@
+class Leet9:
+    def is_palindromenumber(num:int) -> bool:
+        numstring = ""+str(num)
+        rstring = "";
+        for char in numstring:
+            rstring=char+rstring
+        return numstring==rstring
+
+
+
+print(Leet9.is_palindromenumber(121))
+print(Leet9.is_palindromenumber(-121))
+print(Leet9.is_palindromenumber(31213))
